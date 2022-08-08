3a0a3b7c64c215f7566e56a0d48e61fbfc0559a0
RaulFelipeGarcia94 <raulfgarcia10@gmail.com>
2022-08-08 09:16:34 -0300

    Created mask

---
+++ src/utils/masks.ts
@@ -0,0 +1,31 @@
+export const moneyMask = (value: string) => {
+  var tmp = value + ''
+  tmp = tmp.replace(/([0-9]{2})$/g, ',$1')
+  if (tmp.length > 6) tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2')
+
+  return tmp
+}
+
+export const date = (date: string) => {
+  const convert = new Date(date).toLocaleString('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+  })
+  return convert.split(' ')[0]
+}
+
+export const cpfMask = (value: string) => {
+  value = value + ''
+
+  if (value.length <= 11) {
+    value = value.replace(/(\d{3})(\d)/, "$1.$2")
+    value = value.replace(/(\d{3})(\d)/, "$1.$2")
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+  } else {
+    value = value.replace(/^(\d{2})(\d)/, "$1.$2")
+    value = value.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+    value = value.replace(/\.(\d{3})(\d)/, ".$1/$2")
+    value = value.replace(/(\d{4})(\d)/, "$1-$2")
+  }
+
+  return value
+}
