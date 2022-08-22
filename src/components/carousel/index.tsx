be485592a53101f2bcd82f7bdf2013737d205d8b
Murilo <muriloflimaa@gmail.com>
2022-08-22 10:42:01 -0300

    Feature/Carousel

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,34 @@
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+
+export default function CarouselComponent() {
+    return (
+        <div className="carousel-wrapper max-w-7xl mx-auto hidden md:block rounded-xl">
+            <Carousel
+                infiniteLoop
+                useKeyboardArrows
+                autoPlay
+                showStatus={false}
+                showThumbs={false}
+                className="rounded-xl"
+            >
+                <div className="rounded-xl">
+                    <img
+                        src="https://loja.buyphone.com.br/img/banner1.webp"
+                        className="w-full rounded-xl"
+                        alt="Banner 1"
+                        aria-hidden="true"
+                    />
+                </div>
+                <div className="rounded-xl">
+                    <img
+                        src="https://loja.buyphone.com.br/img/banner2.webp"
+                        className="w-full rounded-xl"
+                        alt="Banner 2"
+                        aria-hidden="true"
+                    />
+                </div>
+            </Carousel>
+        </div>
+    )
+}
