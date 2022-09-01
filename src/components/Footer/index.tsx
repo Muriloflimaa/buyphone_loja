451ec0b73cc587cb1536c19fde5b5c3fbb0c84c4
Murilo <muriloflimaa@gmail.com>
2022-09-01 15:54:47 -0300

    Feature/Color Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { MailIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import Image from 'next/image'
 import VisaSvg from '../../assets/images/visa.svg'
@@ -12,6 +12,7 @@ import Parc2Svg from '../../assets/images/image28.svg'
 import Parc3Svg from '../../assets/images/image29.svg'
 import { ICategory } from '../../types'
 
+
 interface FooterProps {
     dataCategory: {
         data: Array<ICategory>
@@ -20,174 +21,130 @@ interface FooterProps {
 
 export default function Footer({ dataCategory }: FooterProps) {
     return (
-        <footer className="bg-colorCard w-full py-6 pb-32 p-4 md:pb-6">
-            <div className="mx-auto max-w-7xl">
-                <div className="py-6">
-                    <div className="h-[1px] w-full bg-white max-w-7xl mx-auto"></div>
-                </div>
-
-                <div className="gap-8 px-6 grid grid-cols-1 lg:grid-cols-5">
-                    <div>
-                        <div className="mx-auto w-full">
-                            <h1 className="text-lg text-PrimaryText">
-                                Departamentos
-                            </h1>
-                            <ul className="text-PrimaryText gap-1 opacity-50">
-                                {dataCategory.data.length > 0 ? (
-                                    dataCategory.data.map((category) => {
-                                        return (
-                                            <li
-                                                className="link"
-                                                key={category.id}
-                                            >
-                                                <Link
-                                                    href={`/${category.slug}`}
-                                                    passHref
-                                                >
-                                                    <a>{category.name}</a>
-                                                </Link>
-                                            </li>
-                                        )
-                                    })
-                                ) : (
-                                    <span>
-                                        Categoria de produtos não disponíveis.
-                                    </span>
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="mx-auto">
-                            <h1 className="text-lg text-PrimaryText">
-                                Mais informaçoes
-                            </h1>
-                            <ul className="text-PrimaryText gap-1 opacity-50">
-                                <li>informações de entrega</li>
-                                <Link href={'/politics'} passHref>
-                                    <li className="link cursor-pointer">
-                                        Política de privacidade
+        <footer className="bg-secondary w-full py-6 pb-32 p-4 md:pb-6">
+            <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row md:justify-between gap-y-12 md:gap-y-0 gap-x-6">
+                <div>
+                    <h3 className="font-bold text-lg mb-3 text-info-content">Categorias</h3>
+                    <ul className="text-info-content gap-1 opacity-90">
+                        {dataCategory.data.length > 0 ? (
+                            dataCategory.data.map((category) => {
+                                return (
+                                    <li className="link" key={category.id}>
+                                        <Link
+                                            href={`/${category.slug}`}
+                                            passHref
+                                        >
+                                            <a>{category.name}</a>
+                                        </Link>
                                     </li>
-                                </Link>
-                                <Link href={'/terms'} passHref>
-                                    <li className="link cursor-pointer">
-                                        Termos e condições
-                                    </li>
-                                </Link>
-                            </ul>
-                        </div>
+                                )
+                            })
+                        ) : (
+                            <span>Categoria de produtos não disponíveis.</span>
+                        )}
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg mb-3 text-info-content">Mais informaçoes</h3>
+                    <ul className="gap-1 opacity-90 text-info-content">
+                        <Link href={'/politics'} passHref>
+                            <li className="link cursor-pointer">
+                                Política de privacidade
+                            </li>
+                        </Link>
+                        <Link href={'/terms'} passHref>
+                            <li className="link cursor-pointer">
+                                Termos e condições
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg mb-3 text-info-content">Contatos</h3>
+                    <ul className="text-sm block leading-6 text-info-content">
+                        <li>
+                            <a
+                                href="mailto:contatobuyphone@gmail.com"
+                                className="flex items-center"
+                            >
+                                <FontAwesomeIcon icon={faEnvelope} className='mr-2 text-info-content' />
+                                contatobuyphone@gmail.com
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.facebook.com/Buyphone.match"
+                                target="_blank"
+                                className="flex items-center"
+                            >
+                                   <FontAwesomeIcon icon={faFacebook} className='mr-2 text-info-content' />
+                                BuyPhone
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://instagram.com/buyphone.match"
+                                target="_blank"
+                                className="flex items-center text-info-content"
+                            >
+                                <FontAwesomeIcon icon={faInstagram} className='mr-2 text-info-content' />
+                                buyphone.match
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex flex-col lg:flex-row gap-12">
+                    <div>
+                        <h3 className="font-bold text-lg mb-3">
+                            Métodos de Pagamento
+                        </h3>
+
+                        <ul className="text-sm flex gap-2">
+                            <li>
+                                <Image src={VisaSvg} layout="fixed"></Image>
+                            </li>
+                            <li>
+                                <Image src={EloSvg} layout="fixed"></Image>
+                            </li>
+                            <li>
+                                <Image src={AESvg} layout="fixed"></Image>
+                            </li>
+                            <li>
+                                <Image src={McSvg} layout="fixed"></Image>
+                            </li>
+                            <li></li>
+                        </ul>
                     </div>
 
                     <div>
-                        <div className="mx-auto">
-                            <h1 className="text-lg text-PrimaryText">
-                                Contatos
-                            </h1>
-                            <ul className="text-sm block leading-6">
-                                <li>
-                                    <a
-                                        href="mailto:contatobuyphone@gmail.com"
-                                        className="flex items-center gap-2"
-                                    >
-                                        <MailIcon className="w-5 h-5" />
-                                        contatobuyphone@gmail.com
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="https://www.facebook.com/Buyphone.match"
-                                        target="_blank"
-                                        className="flex items-center gap-2"
-                                    >
-                                        <FontAwesomeIcon
-                                            className="w-5 h-5"
-                                            icon={faFacebook}
-                                        />
-                                        BuyPhone
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="https://instagram.com/buyphone.match"
-                                        target="_blank"
-                                        className="flex items-center gap-2"
-                                    >
-                                        <FontAwesomeIcon
-                                            className="w-5 h-5"
-                                            icon={faInstagram}
-                                        />
-                                        buyphone.match
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                        <h3 className="font-bold text-lg mb-3">Parceiros</h3>
+                        <ul className="text-sm flex gap-2">
+                            <li>
+                                <Image src={Parc1Svg} layout="fixed"></Image>
+                            </li>
+                            <li>
+                                <Image src={Parc2Svg} layout="fixed"></Image>
+                            </li>
+                            <li>
+                                <Image src={Parc3Svg} layout="fixed"></Image>
+                            </li>
+                            <li></li>
+                        </ul>
+                        <small className="text-xs text-info-content">
+                            Seus dados estão seguros
+                        </small>
                     </div>
+                </div>
+            </div>
 
-                    <div>
-                        <div className="mx-auto">
-                            <h1 className="text-lg text-PrimaryText">
-                                Métodos de pagamento
-                            </h1>
-                            <ul className="text-sm flex flex-col gap-3">
-                                <li>
-                                    <Image src={VisaSvg} layout="fixed"></Image>
-                                </li>
-                                <li>
-                                    <Image src={EloSvg} layout="fixed"></Image>
-                                </li>
-                                <li>
-                                    <Image src={AESvg} layout="fixed"></Image>
-                                </li>
-                                <li>
-                                    <Image src={McSvg} layout="fixed"></Image>
-                                </li>
-                                <li></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="mx-auto">
-                            <h1 className="text-lg text-PrimaryText">
-                                Parcerias
-                            </h1>
-                            <ul className="text-sm flex flex-col gap-3">
-                                <li>
-                                    <Image
-                                        src={Parc1Svg}
-                                        layout="fixed"
-                                    ></Image>
-                                </li>
-                                <li>
-                                    <Image
-                                        src={Parc2Svg}
-                                        layout="fixed"
-                                    ></Image>
-                                </li>
-                                <li>
-                                    <Image
-                                        src={Parc3Svg}
-                                        layout="fixed"
-                                    ></Image>
-                                </li>
-                                <li></li>
-                            </ul>
-                        </div>
-                    </div>
+            <div className="flex flex-col px-6 text-center md:text-start md:flex-row md:justify-between md:items-center text-info-content pt-4">
+                <div>
+                    <h1 className="pt-6">
+                        Copyright BuyPhone - 2022. Todos os direitos reservados.
+                    </h1>
+                    <h2>CNPJ: 40.779.273/0001-09</h2>
                 </div>
-                <div className="py-6">
-                    <div className="h-[1px] w-full bg-white max-w-7xl mx-auto"></div>
-                </div>
-                <div className="flex flex-col px-6 text-center md:text-start md:flex-row md:justify-between md:items-center">
-                    <div>
-                        <h1 className="pt-6">
-                            Copyright BuyPhone - 2022. Todos os direitos
-                            reservados.
-                        </h1>
-                        <h2>CNPJ: 40.779.273/0001-09</h2>
-                    </div>
-                    <h3>Criado com ❤️ por Buyphone.com.br</h3>
-                </div>
+                <h3>Criado com ❤️ por Buyphone.com.br</h3>
             </div>
         </footer>
     )
