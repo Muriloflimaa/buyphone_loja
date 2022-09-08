d6e7e6e64d78011d6ac50d94a5b3af78b607ac82
Murilo <muriloflimaa@gmail.com>
2022-09-08 10:41:22 -0300

    Removed Refresh

--- src/utils/PersistentLogin.ts
+++ src/utils/PersistentLogin.ts
@@ -16,15 +16,8 @@ export function PersistentLogin<P>(fn: GetServerSideProps<any>) {
         const cookies = parseCookies(ctx)
 
         if (cookies['@BuyPhone:Token']) {
+            //se existe um token entrar aqui!
             const decodedToken = jwt_decode<any>(cookies['@BuyPhone:Token']) //decodifica o token
-            const timeElapsed = Date.now() // pega a data de agora
-            const today = new Date(timeElapsed)
-            const d = new Date(0)
-
-            d.setUTCSeconds(decodedToken.exp) // pega a data do token e transforma ela em tempo
-
-            const diff = Math.abs(d.getTime() - today.getTime()) //divide o tempo do token pelo tempo atual
-            const days = Math.ceil(diff / (1000 * 60)) //divide o tempo atual e o tempo restante do token em Min - 60 = 1 Hora
 
             //se existir um token e estiver expirado, mandar para o login
             if (Date.now() >= decodedToken.exp * 1000) {
@@ -33,42 +26,6 @@ export function PersistentLogin<P>(fn: GetServerSideProps<any>) {
                 return {
                     redirect: {
                         destination: '/login',
-                        permanent: false,
-                    },
-                }
-            }
-            //se existir um token e se estiver dentro de 10 minutos para expirar, fazer o refresh e gravar os dados
-            if (days <= 10 && days >= 0) {
-                const config = {
-                    'Content-type': 'application/json',
-                    headers: {
-                        Authorization: `Bearer ${cookies['@BuyPhone:Token']}`,
-                    },
-                }
-
-                const response = await axios.post(
-                    'https://loja.buyphone.com.br/api/refresh',
-                    null,
-                    config
-                )
-
-                // const response = await apiLogin.post('/refresh')
-                const { type, name, id, profile_photo_url } = response.data.user
-
-                const UserObject = {
-                    name: name,
-                    id: id,
-                    type: type,
-                    profile_photo_url: profile_photo_url,
-                }
-                const token = response.data.authorization.token
-
-                setCookies('@BuyPhone:User', UserObject) //chama a função setCookies para gravar os dados
-                setCookies('@BuyPhone:Token', token)
-
-                return {
-                    redirect: {
-                        destination: '/',
                         permanent: false,
                     },
                 }
