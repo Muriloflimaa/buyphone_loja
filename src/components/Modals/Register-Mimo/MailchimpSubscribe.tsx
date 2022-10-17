583140eb6d20d0feee357a7ecfdbd34c5da790be
RaulFelipeGarcia94 <raulfgarcia10@gmail.com>
2022-10-17 11:25:38 -0300

    Updated code

--- src/components/Modals/Register-Mimo/MailchimpSubscribe.tsx
+++ src/components/Modals/Register-Mimo/MailchimpSubscribe.tsx
@@ -1,6 +1,16 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import CustomForm from "./CustomForm";
 
+interface IFormFields {
+    EMAIL: string,
+    FNAME: string,
+    PHONE: string,
+}
+interface ICustomForm {
+    status: string | null
+    subscribe: (data: IFormFields) => void
+}
+
 export default function MailchimpFormContainer() {
 
     const postUrl = `https://buyphone.us21.list-manage.com/subscribe/post?u=7b8f081fe24d9fd579e7c35eb&amp;id=001069f3c1&amp;f_id=0093c2e1f0`;
@@ -9,10 +19,9 @@ export default function MailchimpFormContainer() {
         <>
             <MailchimpSubscribe
                 url={postUrl}
-                render={({ subscribe, status, message }) => (
+                render={({ subscribe, status }: ICustomForm) => (
                     <CustomForm
                         status={status}
-                        message={message}
                         onValidated={formData => subscribe(formData)}
                     />
                 )}
