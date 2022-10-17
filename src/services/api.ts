a95ac937b907ca1be4f85aad9c0cfa22f00006c7
Murilo <muriloflimaa@gmail.com>
2022-10-17 10:41:01 -0300

    feature/link/api

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,8 @@ const { '@BuyPhone:Token': Cookies } = parseCookies()
 
 export const link = 'beta-api.buyphone.com.br'
 
+export const apiStore = setupAPIClient('store')
+
 export function setupAPIClient(endpoint: string) {
   const api = axios.create({
     baseURL: `https://beta-api.buyphone.com.br/${endpoint}`,
@@ -16,5 +18,3 @@ export function setupAPIClient(endpoint: string) {
 
   return api
 }
-
-export const apiStore = setupAPIClient('store')
