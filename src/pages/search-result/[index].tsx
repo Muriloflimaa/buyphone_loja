7bb1865ad16234370d6c3b9e4bb040e2f4e4c635
Murilo <muriloflimaa@gmail.com>
2022-10-21 15:11:32 -0300

    change embed

--- src/pages/search-result/[index].tsx
+++ src/pages/search-result/[index].tsx
@@ -40,68 +40,80 @@ export default function SearchResult() {
   return (
     <>
       {products.length > 0 && (
-        <div className="grid grid-cols-2  md:grid-cols-4 mx-auto gap-6 px-5 md:px-0 max-w-7xl py-24 md:py-10">
-          {products &&
-            products.map((products: IProduct) => {
-              const discount =
-                process.env.NEXT_PUBLIC_BLACK_FRIDAY &&
-                !!JSON.parse(process.env.NEXT_PUBLIC_BLACK_FRIDAY)
-                  ? 12.5
-                  : !!isUser && user && JSON.parse(user)?.type === 1
-                  ? 12.5
-                  : 7
-              const itens = [
-                products.price,
-                products.magalu_price,
-                products.americanas_price,
-                products.casasbahia_price,
-                products.ponto_price,
-              ]
-              const filteredItens = itens.filter((item) => item)
-              const averagePrice =
-                filteredItens.length > 0 ? Math.min(...filteredItens) : 0
-              const discountPrice = Math.round(averagePrice * (discount / 100))
-              const ourPrice = averagePrice - discountPrice //realiza a verificacao de preco, nao foi possivel usar a existente
-
-              return (
-                ourPrice > 0 && (
-                  <ProductCard
-                    key={products.id}
-                    id={products.id}
-                    name={products.name}
-                    idCategory={products.category_id}
-                    colorPhone={products.color}
-                    price={ourPrice}
-                    averagePrice={averagePrice}
-                    slug={products.slug}
-                    slugCategory={products.slug}
-                    image={products.media[0].original_url}
-                    memory={products.memory}
-                  />
+        <>
+          <h1 className="text-4xl font-medium text-center">
+            Você buscou por {result}
+          </h1>
+          <div className="grid grid-cols-2  md:grid-cols-4 mx-auto gap-6 px-5 md:px-0 max-w-7xl py-24 md:py-10">
+            {products &&
+              products.map((products: IProduct) => {
+                const discount =
+                  process.env.NEXT_PUBLIC_BLACK_FRIDAY &&
+                  !!JSON.parse(process.env.NEXT_PUBLIC_BLACK_FRIDAY)
+                    ? 12.5
+                    : !!isUser && user && JSON.parse(user)?.type === 1
+                    ? 12.5
+                    : 7
+                const itens = [
+                  products.price,
+                  products.magalu_price,
+                  products.americanas_price,
+                  products.casasbahia_price,
+                  products.ponto_price,
+                ]
+                const filteredItens = itens.filter((item) => item)
+                const averagePrice =
+                  filteredItens.length > 0 ? Math.min(...filteredItens) : 0
+                const discountPrice = Math.round(
+                  averagePrice * (discount / 100)
                 )
-              )
-            })}
-        </div>
+                const ourPrice = averagePrice - discountPrice //realiza a verificacao de preco, nao foi possivel usar a existente
+
+                return (
+                  ourPrice > 0 && (
+                    <ProductCard
+                      key={products.id}
+                      id={products.id}
+                      name={products.name}
+                      idCategory={products.category_id}
+                      colorPhone={products.color}
+                      price={ourPrice}
+                      averagePrice={averagePrice}
+                      slug={products.slug}
+                      slugCategory={products.slug}
+                      image={products.media[0].original_url}
+                      memory={products.memory}
+                    />
+                  )
+                )
+              })}
+          </div>
+        </>
       )}
       {!!isError && (
-        <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 px-4 py-24 md:py-10">
-          <div>
-            <Image src={JuninhoImg} layout="fixed" width={200} height={250} />
+        <>
+          <h1 className="text-4xl font-medium text-center">
+            Você buscou por {result}
+          </h1>
+          <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 px-4 py-24 md:py-10">
+            <div>
+              <Image src={JuninhoImg} layout="fixed" width={200} height={250} />
+            </div>
+            <div className="mt-4">
+              <span className="text-lg text-info-content">
+                <span className="font-semibold text-3xl">Ops!</span>
+                <p>Não encontramos nenhum resultado para a busca por</p>
+                <p className="font-medium text-lg">"{result}"</p>
+              </span>
+              <ul className="list-disc pl-4 mt-4 text-red-600">
+                <li>Verifique se a palavra foi digitada corretamente.</li>
+                <li>Tente palavras menos específicas.</li>
+                <li>Tente palavras-chave diferentes.</li>
+                <li>Faça buscas relacionadas.</li>
+              </ul>
+            </div>
           </div>
-          <div className="mt-4">
-            <span className="text-lg text-info-content">
-              <span className="font-semibold text-3xl">Ops!</span>
-              <p>Não encontramos nenhum resultado para a busca por</p>
-              <p className="font-medium text-lg">"{result}"</p>
-            </span>
-            <ul className="list-disc pl-4 mt-4 text-red-600">
-              <li>Verifique se a palavra foi digitada corretamente.</li>
-              <li>Tente palavras menos específicas.</li>
-              <li>Tente palavras-chave diferentes.</li>
-              <li>Faça buscas relacionadas.</li>
-            </ul>
-          </div>
-        </div>
+        </>
       )}
     </>
   )
