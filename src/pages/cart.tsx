27211d4678287197e5e3059ba90acf1004d95768
Murilo <muriloflimaa@gmail.com>
2022-11-03 12:47:40 -0300

    added verification qtt product cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -106,56 +106,58 @@ export default function Cart() {
           )}
         </div>
       </div>
-      <div className="flex w-full justify-end max-w-7xl my-10 px-4">
-        <div className="flex flex-col max-w-md w-full">
-          {userData?.promotion && (
-            <div className="flex justify-between">
-              <span className="text-info-content text-lg">Desconto:</span>
-              <span className="font-semibold text-lg text-green-600">
-                R$ - 150,00
-              </span>
-            </div>
-          )}
+      {cartSize && cartSize > 0 && (
+        <div className="flex w-full justify-end max-w-7xl my-10 px-4">
+          <div className="flex flex-col max-w-md w-full">
+            {userData?.promotion && (
+              <div className="flex justify-between">
+                <span className="text-info-content text-lg">Desconto:</span>
+                <span className="font-semibold text-lg text-green-600">
+                  R$ - 150,00
+                </span>
+              </div>
+            )}
 
-          <div className="flex justify-between">
-            <span className="text-gray-500 text-sm">Subtotal</span>
-            <span className="text-sm text-gray-500">
-              R$ {moneyMask((somaTotal + discountValue).toString())}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span className="text-gray-500 text-sm">Frete</span>
-            <span className="text-sm text-success font-semibold">Grátis</span>
-          </div>
-          {userData?.promotion && (
             <div className="flex justify-between">
-              <span className="text-gray-500 text-sm">Desconto:</span>
-              <span className="font-semibold text-sm text-green-600">
-                R$ -150,00
+              <span className="text-gray-500 text-sm">Subtotal</span>
+              <span className="text-sm text-gray-500">
+                R$ {moneyMask((somaTotal + discountValue).toString())}
               </span>
             </div>
-          )}
+            <div className="flex justify-between">
+              <span className="text-gray-500 text-sm">Frete</span>
+              <span className="text-sm text-success font-semibold">Grátis</span>
+            </div>
+            {userData?.promotion && (
+              <div className="flex justify-between">
+                <span className="text-gray-500 text-sm">Desconto:</span>
+                <span className="font-semibold text-sm text-green-600">
+                  R$ -150,00
+                </span>
+              </div>
+            )}
 
-          <Divider className="h-[1px] bg-black/50" />
-          {
-            <div className="flex w-full justify-between items-center">
-              <span>Total</span>
-              <span className="font-semibold text-info-content text-lg">
-                R$ {moneyMask(somaTotal?.toString())}
-              </span>
-            </div>
-          }
-          {cartSize && cartSize > 0 ? (
-            <div className="w-full flex justify-end mt-2">
-              <Link href={'/shipping'}>
-                <a className="btn w-64 btn-success font-medium normal-case">
-                  Finalizar Compra
-                </a>
-              </Link>
-            </div>
-          ) : null}
+            <Divider className="h-[1px] bg-black/50" />
+            {
+              <div className="flex w-full justify-between items-center">
+                <span>Total</span>
+                <span className="font-semibold text-info-content text-lg">
+                  R$ {moneyMask(somaTotal?.toString())}
+                </span>
+              </div>
+            }
+            {cartSize && cartSize > 0 ? (
+              <div className="w-full flex justify-end mt-2">
+                <Link href={'/shipping'}>
+                  <a className="btn w-64 btn-success font-medium normal-case">
+                    Finalizar Compra
+                  </a>
+                </Link>
+              </div>
+            ) : null}
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
