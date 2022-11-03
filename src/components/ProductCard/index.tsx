f33ec5eaa41b07d8e70089439224c74d4d458ef1
Murilo <muriloflimaa@gmail.com>
2022-11-03 10:58:43 -0300

    added allert discount product

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -21,6 +21,7 @@ interface ProductCardProps {
   idCategory: number
   slug: string
   slugCategory: string
+  changeText: boolean
 }
 
 const ProductCard = ({
@@ -33,6 +34,7 @@ const ProductCard = ({
   slug,
   slugCategory,
   memory,
+  changeText,
 }: ProductCardProps) => {
   const [color, setColor] = useState<string | undefined>()
   const { addProduct } = useCart()
@@ -50,109 +52,102 @@ const ProductCard = ({
   const router = useRouter()
 
   return (
-    <>
-      <div
-        className="card card-compact cursor-pointer w-full h-full text-info-content bg-accent shadow-black md:hover:shadow-2xl md:hover:drop-shadow-lg md:hover:scale-105 transition-all duration-200 sm:card-normal max-w-xs relative overflow-visible rounded-lg"
-        key={id}
-      >
-        <div className="card-body text-center flex flex-col justify-between">
-          <div>
-            <div onClick={() => router.push(link)} className="w-[80%] mx-auto">
-              <figure className="mb-6">
-                <Image src={image} width={350} height={450} />
-              </figure>
-            </div>
-            <div>
-              <h2
-                onClick={() => router.push(link)}
-                className="card-title justify-center flex-col font-medium"
-              >
-                {name}
-              </h2>
-              <div
-                onClick={() => router.push(link)}
-                className="flex flex-wrap gap-2 justify-center"
-              >
-                <div className="badge badge-outline text-xs h-auto">
-                  <div
-                    className={`h-2 w-2 rounded-full mr-2 border border-black ${color}`}
-                  ></div>
-                  <span className="w-max">{colorPhone}</span>
-                </div>
-                <span className="badge badge-outline text-xs">{memory}</span>
-              </div>
-            </div>
-          </div>
-          {/* //para desktok */}
-          {price > 0 && (
-            <div
-              onClick={() => router.push(link)}
-              className="flex-col mb-3 text-left my-6 gap-2 hidden lg:flex"
-            >
-              <div className="flex justify-between items-center text-xs">
-                <span>Preço comum:</span>
-                <span className="opacity-80 line-through decoration-red-600">
-                  R$ {moneyMask(averagePrice.toString())}
-                </span>
-              </div>
-              <div className="flex justify-between items-center text-base">
-                <span className="text-xs">Nosso Preço:</span>
-                <span className="text-success font-semibold">
-                  R${moneyMask(price.toString())}
-                </span>
-              </div>
-            </div>
-          )}
-
-          {/* //para mobile */}
-          {price > 0 && (
-            <div
-              onClick={() => router.push(link)}
-              className="flex flex-col gap-1 lg:hidden"
-            >
-              <div className="flex flex-col">
-                <span className="text-xs">Preço comum:</span>
-                <span className="opacity-40 line-through decoration-red-600">
-                  {moneyMask(averagePrice.toString())}
-                </span>
-              </div>
-              <div className="flex flex-col">
-                <span className="text-xs">Nosso preço: </span>
-                <span className="text-success font-semibold text-base">
-                  R$ {moneyMask(price.toString())}
-                </span>
-              </div>
-            </div>
-          )}
-          {price > 0 && (
-            <div className="card-actions flex-col items-center">
-              <button
-                className="btn btn-info text-white w-full upper-case text-xs md:text-md py-0"
-                data-testid="add-product-button"
-                onClick={() => handleAddProduct(id)}
-              >
-                Adicionar
-              </button>
-            </div>
-          )}
-          {price <= 0 && (
-            <label htmlFor={id.toString()}>
-              <div className="flex flex-col cursor-pointer p-3 bg-slate-200 transition-all duration-300 rounded-lg hover:bg-slate-100">
-                <h1 className="text-xs">Produto indisponível no momento</h1>
-                <div className="flex flex-col md:flex-row gap-1 text-blue-500 items-center justify-center">
-                  <FontAwesomeIcon icon={faBell} className="w-3 h-3" />
-                  <h2 className="link text-xs font-semibold">
-                    Avise-me quando chegar
-                  </h2>
-                </div>
-              </div>
-            </label>
-          )}
-
-          <Link href={link} passHref>
-            <a className="text-xs link">Saiba mais</a>
-          </Link>
+    <div
+      className="card card-compact cursor-pointer w-full h-full text-info-content bg-accent shadow-black md:hover:shadow-2xl md:hover:drop-shadow-lg md:hover:scale-105 transition-all duration-200 sm:card-normal max-w-xs relative overflow-visible rounded-lg"
+      key={id}
+    >
+      <div className="card-body text-center ">
+        <div onClick={() => router.push(link)} className="w-[80%] mx-auto">
+          <figure className="mb-6">
+            <Image src={image} width={350} height={450} />
+          </figure>
         </div>
+
+        <span className="badge badge-success w-full bg-[#F8F5BD] text-[#BF7300] uppercase text-xs font-semibold mx-auto">
+          {`${changeText ? '7% de desconto' : 'parcelamento em ate 12x'}`}
+        </span>
+        <span className="badge badge-success w-full bg-[#D5FDC7] text-[#004907] uppercase text-xs font-semibold mx-auto">
+          {changeText ? (
+            <span className="flex gap-1">
+              <span className="text-[#1F7501]/50">Economia de </span>
+
+              <span className="text-[#004907]">
+                R$ {moneyMask((averagePrice - price).toString())}
+              </span>
+            </span>
+          ) : (
+            <span className="flex gap-1">
+              <span className="text-[#004907]">Entrada no PIX + </span>
+              <span className="text-[#1F7501]/50">parcelas</span>
+            </span>
+          )}
+        </span>
+        <h2
+          onClick={() => router.push(link)}
+          className="card-title justify-center flex-col font-medium"
+        >
+          {name}
+        </h2>
+        <div
+          onClick={() => router.push(link)}
+          className="flex flex-wrap gap-2 justify-center"
+        >
+          <div className="badge badge-outline text-xs h-auto">
+            <div
+              className={`h-2 w-2 rounded-full mr-2 border border-black ${color}`}
+            ></div>
+            <span className="w-max">{colorPhone}</span>
+          </div>
+          <span className="badge badge-outline text-xs">{memory}</span>
+        </div>
+        {/* //para desktop */}
+        <div
+          onClick={() => router.push(link)}
+          className="flex-col mb-3 text-left my-6 gap-2 hidden lg:flex"
+        >
+          <div className="flex justify-between items-center text-xs">
+            <span>Preço comum:</span>
+            <span className="opacity-80 line-through decoration-red-600">
+              R$ {moneyMask(averagePrice.toString())}
+            </span>
+          </div>
+          <div className="flex justify-between items-center text-base">
+            <span className="text-xs">Nosso Preço:</span>
+            <span className="text-success font-semibold">
+              R${moneyMask(price.toString())}
+            </span>
+          </div>
+        </div>
+        {/* //para mobile */}
+        <div
+          onClick={() => router.push(link)}
+          className="flex flex-col gap-1 lg:hidden"
+        >
+          <div className="flex flex-col">
+            <span className="text-xs">Preço comum:</span>
+            <span className="opacity-40 line-through decoration-red-600">
+              {moneyMask(averagePrice.toString())}
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs">Nosso preço: </span>
+            <span className="text-success font-semibold text-base">
+              R$ {moneyMask(price.toString())}
+            </span>
+          </div>
+        </div>
+        <div className="card-actions flex-col items-center">
+          <button
+            className="btn btn-info text-white w-full upper-case text-xs md:text-md py-0"
+            data-testid="add-product-button"
+            onClick={() => handleAddProduct(id)}
+          >
+            Adicionar
+          </button>
+        </div>
+        <Link href={link} passHref>
+          <a className="text-xs link">Saiba mais</a>
+        </Link>
       </div>
       {/* modal */}
       {price <= 0 && (
@@ -192,7 +187,7 @@ const ProductCard = ({
           </label>
         </>
       )}
-    </>
+    </div>
   )
 }
 export default ProductCard
