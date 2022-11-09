d450a3296a4416174d45286e7e065fe7c00b1956
Murilo <muriloflimaa@gmail.com>
2022-11-09 09:54:16 -0300

    features page cart

--- src/components/ProductCart2/index.tsx
+++ src/components/ProductCart2/index.tsx
@@ -138,117 +138,110 @@ const ProductCart = ({
   }
 
   return (
-    <div
-      className={
-        'bg-colorCard rounded-xl w-full h-min flex justify-between text-xs flex-col  '
-      }
-      key={id}
-    >
-      <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="grid col-span-2 gap-3 text-primary">
-          <div className="flex items-center gap-2">
-            <div className="w-36 h-full flex items-center">
-              <img src={image} alt={name} />
-            </div>
+    <div className="grid grid-cols-1 md:grid-cols-3 justify-between w-full">
+      <div className="grid col-span-2 gap-3 text-primary">
+        <div className="flex items-center gap-2">
+          <div className="w-36 h-full flex items-center">
+            <img src={image} alt={name} />
+          </div>
 
-            <div className="flex flex-col items-start gap-8 h-full">
-              <div className="flex flex-col text-info-content">
-                <span>Modelo</span>
-                <strong className="text-xl">{`${name} (${color}, ${memory})`}</strong>
-              </div>
-              <div className="flex items-center gap-2 text-info-content">
-                <span>Quantidade</span>
-                {!show && (
-                  <div className="btn-group max-h-8 shadow-sm shadow-black/20 rounded-lg">
-                    <button
-                      className="btn btn-accent min-h-0 bg-[#9A9A9A]/10 text-xs h-auto p-2"
-                      type="button"
-                      disabled={amount <= 1}
-                      onClick={() => handleProductDecrement(id, amount)}
-                    >
-                      -
-                    </button>
-                    <button className="btn btn-accent min-h-0 font-normal text-xs h-auto p-2">
-                      {' '}
-                      {amount}
-                    </button>
-                    <button
-                      className="btn btn-accent min-h-0 bg-[#9A9A9A]/10 text-xs h-auto p-2"
-                      type="button"
-                      onClick={() => handleProductIncrement(id, amount)}
-                    >
-                      +
-                    </button>
-                  </div>
-                )}
-                {!show && (
+          <div className="flex flex-col items-start gap-8 h-full">
+            <div className="flex flex-col text-info-content">
+              <span>Modelo</span>
+              <strong className="text-xl">{`${name} (${color}, ${memory})`}</strong>
+            </div>
+            <div className="flex items-center gap-2 text-info-content">
+              <span>Quantidade</span>
+              {!show && (
+                <div className="btn-group max-h-8 shadow-sm shadow-black/20 rounded-lg">
                   <button
+                    className="btn btn-accent min-h-0 bg-[#9A9A9A]/10 text-xs h-auto p-2"
                     type="button"
-                    data-testid="remove-product"
-                    onClick={() => handleRemoveProduct(id, name, color, memory)}
+                    disabled={amount <= 1}
+                    onClick={() => handleProductDecrement(id, amount)}
                   >
-                    <TrashIcon className="h-4 w-4 text-red-600" />
+                    -
                   </button>
-                )}
-              </div>
+                  <button className="btn btn-accent min-h-0 font-normal text-xs h-auto p-2">
+                    {' '}
+                    {amount}
+                  </button>
+                  <button
+                    className="btn btn-accent min-h-0 bg-[#9A9A9A]/10 text-xs h-auto p-2"
+                    type="button"
+                    onClick={() => handleProductIncrement(id, amount)}
+                  >
+                    +
+                  </button>
+                </div>
+              )}
+              {!show && (
+                <button
+                  type="button"
+                  data-testid="remove-product"
+                  onClick={() => handleRemoveProduct(id, name, color, memory)}
+                >
+                  <TrashIcon className="h-4 w-4 text-red-600" />
+                </button>
+              )}
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-2 justify-center items-start md:items-end">
-          <div>
-            <div className="p-3 items-start w-full flex flex-col text-info-content">
-              <h1 className="text-base font-semibold">
-                Calcule o frete e prazo de entrega
-              </h1>
-              <form
-                className="flex flex-col md:flex-row items-start gap-2"
-                onSubmit={handleSubmit(handleCepStorage)}
-              >
-                <Input
-                  {...register('cep')}
-                  type="text"
-                  maxLength={9}
-                  placeholder="00000-000"
-                  onKeyUp={(e) => mascaraCep(e.target, '#####-####')}
-                  error={errors.cep}
-                />
-                {formState.isSubmitting ? (
-                  <button
-                    type="submit"
-                    className="btn loading normal-case text-white"
-                  >
-                    Carregando
-                  </button>
-                ) : (
-                  <button
-                    className="btn btn-info text-white upper-case text-xs md:text-md py-0"
-                    type="submit"
-                  >
-                    Consultar
-                  </button>
-                )}
-              </form>
-              <div className="flex flex-col gap-2 mt-2 items-start md:text-xs">
-                {address && (
-                  <div className="flex items-center gap-2">
-                    <FontAwesomeIcon icon={faLocationDot} className="w-4 h-4" />
-                    <p>
-                      {`${address?.Street && address?.Street + ' -'} ${
-                        address?.City
-                      }, ${address?.UF}`}
-                    </p>
-                  </div>
-                )}
+      </div>
+      <div className="flex flex-col gap-2 justify-center items-start md:items-end">
+        <div>
+          <div className="p-3 items-start w-full flex flex-col text-info-content">
+            <h1 className="text-base font-semibold">
+              Calcule o frete e prazo de entrega
+            </h1>
+            <form
+              className="flex flex-col md:flex-row items-start gap-2"
+              onSubmit={handleSubmit(handleCepStorage)}
+            >
+              <Input
+                {...register('cep')}
+                type="text"
+                maxLength={9}
+                placeholder="00000-000"
+                onKeyUp={(e) => mascaraCep(e.target, '#####-####')}
+                error={errors.cep}
+              />
+              {formState.isSubmitting ? (
+                <button
+                  type="submit"
+                  className="btn loading normal-case text-white"
+                >
+                  Carregando
+                </button>
+              ) : (
+                <button
+                  className="btn btn-info text-white upper-case text-xs md:text-md py-0"
+                  type="submit"
+                >
+                  Consultar
+                </button>
+              )}
+            </form>
+            <div className="flex flex-col gap-2 mt-2 items-start md:text-xs">
+              {address && (
+                <div className="flex items-center gap-2">
+                  <FontAwesomeIcon icon={faLocationDot} className="w-4 h-4" />
+                  <p>
+                    {`${address?.Street && address?.Street + ' -'} ${
+                      address?.City
+                    }, ${address?.UF}`}
+                  </p>
+                </div>
+              )}
 
-                {shippingOn && !shippingOn.error && (
-                  <div className="flex justify-between items-start w-full text-success">
-                    <p className="flex items-center gap-2">
-                      <FontAwesomeIcon icon={faTruckFast} className="w-4 h-4" />
-                      {`Chegará grátis em até ${shippingOn?.days} dias úteis`}
-                    </p>
-                  </div>
-                )}
-              </div>
+              {shippingOn && !shippingOn.error && (
+                <div className="flex justify-between items-start w-full text-success">
+                  <p className="flex items-center gap-2">
+                    <FontAwesomeIcon icon={faTruckFast} className="w-4 h-4" />
+                    {`Chegará grátis em até ${shippingOn?.days} dias úteis`}
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </div>
