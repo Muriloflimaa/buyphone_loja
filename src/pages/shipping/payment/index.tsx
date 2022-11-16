a4f12c1a16bd11914fdd30a1a57847e8e2851a0c
Murilo <muriloflimaa@gmail.com>
2022-11-16 16:33:05 -0300

    features authcontext

--- src/pages/shipping/payment/index.tsx
+++ src/pages/shipping/payment/index.tsx
@@ -80,24 +80,20 @@ export default function payment() {
                   </span>
                 </div>
                 <div>
-                  <a
-                    href={`https://api.whatsapp.com/send?phone=5518981367275&text=Ol%C3%A1!%20Eu%20gostaria%20de%20saber%20mais%20sobre%20o%20m%C3%A9todo%20de%20pagamento%20personalizado.%20Meu%20Carrinho:%20${
-                      setDat && JSON.stringify(setDat)
-                    }`}
-                    target="_blank"
-                    className="btn btn-info text-white w-full justify-between"
-                  >
-                    <span>Personalizado</span>
-                    <svg
-                      className="h-6 w-6 fill-current md:h-8 md:w-8"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
-                    </svg>
-                  </a>
+                  <Link href={'/shipping/payment/custom'}>
+                    <a className="btn btn-info text-white w-full justify-between">
+                      <span>Personalizado</span>
+                      <svg
+                        className="h-6 w-6 fill-current md:h-8 md:w-8"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
+                      </svg>
+                    </a>
+                  </Link>
                   <span className="mt-2 block text-xs font-normal">
                     • Nesta forma de pagamento você poderá dar uma entrada no
                     Pix e parcelar o restante. Assim você economiza no valor
